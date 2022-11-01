f46dfcd23c8e98e638013402af050821e5d6f0ce
Seraj Mostafa <32425034+sammgithub@users.noreply.github.com>
2022-11-01 16:02:02 -0400

    updated with collocation_dask_local.py

--- examples/collocate_viirs_calipso_dask_aws/service_1_trigger_lambda_to_execute.py
+++ examples/collocate_viirs_calipso_dask_aws/service_1_trigger_lambda_to_execute.py
@@ -1,14 +1,14 @@
 import json, boto3, time, sys, os
 
 credentials = ["us-west-2","your-access-key","your-secret-key"]   #region,access_key,secret_key
-InstanceId = 'i-0b83239ada0a8ae1d' ## EC2 instance ID
+InstanceId = 'i-0b83239ada0a8ae1d' ## change to your EC2 instance ID
 
 def get_ec2_instances_id(region,access_key,secret_key):
     ec2_conn = boto3.resource('ec2',region_name=region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
     
     if ec2_conn:
         for instance in ec2_conn.instances.all():
-            if instance.state['Name'] == 'running' and instance.security_groups[0]['GroupName'] == 'your-security-group-name': ## i.e., default, distributed_dl
+            if instance.state['Name'] == 'running' and instance.security_groups[0]['GroupName'] == 'launch-wizard-13':
                 masterInstanceId = instance.instance_id
                 print("Master Instance Id is ",masterInstanceId)
         return masterInstanceId
@@ -18,16 +18,12 @@ def get_ec2_instances_id(region,access_key,secret_key):
 
 def send_command_to_master(InstanceId,command,ssm_client):
     response = ssm_client.send_command(InstanceIds=[InstanceId],DocumentName="AWS-RunShellScript",Parameters={'commands': [command]})
-    
     cmdId = response['Command']['CommandId']
     return cmdId
-
+    
 def lambda_handler(event, context):
     masterInstanceId = InstanceId 
     ssm_client = boto3.client('ssm',region_name=credentials[0],aws_access_key_id=credentials[1],aws_secret_access_key=credentials[2])
-    
-    output = send_command_to_master(masterInstanceId,\
-        "cd /home/ec2-user/DL_3d_cloud_retrieval-main && mkdir -p outputs && /usr/bin/python3.8 COT_retrieval/test_example.py --cot_file_name=example/training_data/data_cot.h5 --path_1d_retrieval=example/retrieved_COT/ --path_model=saved_model/lstm/model1.h5 --path_predictions=outputs/ --radiance_test=example/testing_data/X_test_1.npy --cot_test=example/testing_data/y_test_1.npy --path_plots=outputs/",\
+    return send_command_to_master(masterInstanceId,\
+        "cd /home/ubuntu/satellite_collocation-main && /usr/bin/python3 examples/collocate_viirs_calipso_dask_cluster/collocation_dask_local.py -tp ../satellite_collocation_sample_data/CALIPSO-L2-01km-CLayer/ -sgp ../satellite_collocation_sample_data/VNP03MOD-VIIRS-Coordinates/ -sdp ../satellite_collocation_sample_data/VNP02MOD-VIIRS-Attributes/ -sp ../satellite_collocation_sample_data/collocation-output/",\
         ssm_client)
-        
-    return 'https://jldgsfljne.execute-api.us-west-2.amazonaws.com/default/seraj_call_url_long_services?command_id='+output ## the https//...is the service2 API called by this service1 API
