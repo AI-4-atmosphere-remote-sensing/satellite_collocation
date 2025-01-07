55e95f1450f2f1ed79e4283ff5189b1cf3b136bc
lixy4567 <90643297+lixy4567@users.noreply.github.com>
2025-01-07 09:54:53 -0500

    Update extract_abi_caliop_collocated_data.py
    Add ABI angles calculation

--- examples/collocate_abi_calipso_local_execution/extract_model_training_features/extract_abi_caliop_collocated_data.py
+++ examples/collocate_abi_calipso_local_execution/extract_model_training_features/extract_abi_caliop_collocated_data.py
@@ -19,21 +19,121 @@ warnings.filterwarnings("ignore")
 import argparse
 import string
 
-parser = argparse.ArgumentParser(description='This code is an example of extracting ABI data based on existing index files')
-parser.add_argument('-ap','--abi_path', help='Define the path of ABI files', required=True)
-parser.add_argument('-apl2','--abil2_path', help='Define the path of ABI L2 files', required=True)
-parser.add_argument('-ip','--index_path', help='Define the path of Index files', required=True)
-parser.add_argument('-sp','--save_path', help='Define the path of output files', required=True)
-
-args = vars(parser.parse_args())
-abi_file_path = args['abi_path'].strip()
-abi_file_l2_path = args['abil2_path'].strip()
-index_path = args['index_path'].strip()
-save_path = args['save_path'].strip()
+abi_file_path = '/Volumes/MyPassport/abi_l1_201711/'
+abi_file_l2_path = '/Volumes/MyPassport/abi_l2_201711/'
+index_path = '/Volumes/MyPassport/2017_index/index_11_part/'
+save_path = '/Users/xingyanli/Lab/ACCESS/satellite_collocation/examples/collocate_abi_calipso_local_execution/extracted_data/'
 
 abi_files = sorted(glob.glob(abi_file_path+'/*/'+'/*/'+'OR_ABI-L1b-RadF-M?C01*.nc'))
 abi_ranges = []
 abi_stimeflags = []
+
+
+# Functions for calculating solar and sensor angles
+def Solar_Calculate(xlon, xlat, jday, tu):
+    pi = 3.14159265
+    dtor = pi / 180.0                         # change degree to radians
+    
+    tsm = tu + xlon/15.0                      # mean solar time
+    xlo = xlon*dtor
+    xla = xlat*dtor
+    xj = jday
+    
+    a1 = (1.00554*xj - 6.28306)  * dtor
+    a2 = (1.93946*xj + 23.35089) * dtor
+    et = -7.67825*np.sin(a1) - 10.09176*np.sin(a2)             #time equation
+    
+    tsv = tsm + et/60.0 
+    tsv = tsv - 12.0                                    # true solar time
+    
+    ah = tsv*15.0*dtor                                 # hour angle
+    
+    a3 = (0.9683*xj - 78.00878) * dtor
+    delta = 23.4856*np.sin(a3)* dtor                    # solar declination (in radian)
+
+    cos_delta = np.cos(delta)
+    sin_delta = np.sin(delta)
+    cos_ah = np.cos(ah)
+    sin_xla = np.sin(xla)
+    cos_xla = np.cos(xla)
+    
+    
+    amuzero = sin_xla*sin_delta + cos_xla*cos_delta*cos_ah
+    elev = np.arcsin(amuzero)
+    cos_elev = np.cos(elev)
+    az = cos_delta*np.sin(ah)/cos_elev
+    caz = (-cos_xla*sin_delta + sin_xla*cos_delta*cos_ah) / cos_elev
+       
+    if (az >= 1.0):
+        azim = np.arcsin(1.0)
+    elif (az <= -1.0):
+        azim = np.arcsin(-1.0)
+    else:
+        azim = np.arcsin(az)
+ 
+    if (caz <= 0.0):
+        azim = pi - azim
+               
+    if ((caz > 0.0) & (az <= 0.0)):
+        azim = 2 * pi + azim
+        
+    azim = azim + pi
+    pi2 = 2 * pi
+    
+    if (azim > pi2):
+        azim = azim - pi2
+   
+    #conversion in degrees
+    elev = elev / dtor
+    asol = 90.0 - elev                                 # solar zenith angle in degrees
+    
+    print('SZA is:', asol)
+    # print('------------------------------------------------')
+    
+    phis1 = azim / dtor - 180.0                         # solar azimuth angle in degrees
+    phis2 = phis1 + 360
+    
+    # print('SAA is:', phis1)
+    # print('SAA is:', phis2)
+    # print('------------------------------------------------')
+    phis = phis2 - 180
+    print('SAA is (for libradtran SSA is from south):', phis)
+
+    return asol, phis1, phis
+
+def Sensor_Calculate(xlon, xlat):
+    pi = 3.14159265
+    dtor = pi / 180.0                         # change degree to radians
+    
+    satlon = -137.0
+    satlat = 0.0
+    
+    lon = (xlon - satlon) * dtor   # in radians
+#     print('Lon is:',lon)
+    lat = (xlat - satlat) * dtor   # in radians
+#     print('Lat is:',lat)
+
+    beta = np.arccos(np.cos(lat) * np.cos(lon))
+    sin_beta = np.sin(beta)
+
+    # zenith angle    
+    x = 42164.0* sin_beta/np.sqrt(1.808e09 - 5.3725e08*np.cos(beta))
+    zenith = np.arcsin(x)
+    zenith = zenith/dtor
+    print('VZA is:',zenith)
+
+    # azimuth angle
+    azimuth = np.sin(lon) / sin_beta
+    azimuth = np.arcsin(azimuth)
+    azimuth = azimuth / dtor
+    if (lat < 0.0):
+        azimuth = 180.0 - azimuth
+    if (azimuth < 0.0):
+        azimuth = azimuth + 360.0
+    print('VAA is:',azimuth)
+    
+    return zenith, azimuth
+
 
 for abi_file in abi_files:
     abi_name = os.path.basename(abi_file)
@@ -118,7 +218,6 @@ for index_file in index_files:
         for channel in save_channel:
             current_abi_files = sorted(glob.glob(abi_file_path+'/*/'+'/*/'+'OR_ABI-L1b-RadF-M?C' + str(channel).zfill(2) + '*_s'+abi_stimeflag+'*.nc'))
             if (len(current_abi_files)!=1):
-                print("ABI L1 data is missing for this timeflag.")
                 continue
             current_abi_file = current_abi_files[0]
             abi_name = os.path.basename(current_abi_file)
@@ -145,10 +244,6 @@ for index_file in index_files:
         
         # Load ABI L2 file at this timeframe OR_ABI-L2-ACTPF-M3_G16_s20173050000407_e20173050011174_c20173050011455
         current_abi_l2_files = sorted(glob.glob(abi_file_l2_path+'/*/'+'/*/'+'OR_ABI-L2-ACTPF-M' + '*_s'+abi_stimeflag+'*.nc'))
-        if (len(current_abi_l2_files)!=1):
-            print("ABI L2 data is missing for this timeflag.")
-            continue
-
         current_abi_l2_file = current_abi_l2_files[0]
         abi_ind_meridional_l2 = abi_ind_meridional_2km[index]
         abi_ind_zonal_l2 = abi_ind_zonal_2km[index]
@@ -160,15 +255,77 @@ for index_file in index_files:
         abi_cloud[index] = cloud_phase[abi_ind_meridional_l2,abi_ind_zonal_l2]
         dataset.close()
 
+    # Calculate ABI angles
+    ABI_Lat_1km = fid['ABI_Lat_1km'][:]
+    ABI_Lon_1km = fid['ABI_Lon_1km'][:]
+    ABI_Timeflag = select_abi_stimeflag
+
+    print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print(f'Starting with ABI Timeflag:{ABI_Timeflag}')
+
+    SoZeAn = []
+    SoAzAn = []
+    SeZeAn = []
+    SeAzAn = []
+    minute = []
+
+    for i in range(len(ABI_Timeflag)):
+        timeflag = str(ABI_Timeflag[i])
+        if timeflag=='0':
+            SoZeAn.append(-9999.0)
+            SoAzAn.append(-9999.0)
+            SeZeAn.append(-9999.0)
+            SeAzAn.append(-9999.0)
+            minute.append(-9999.0)
+            print('No ABI data available at the current pixel.')
+        else:
+            lon = ABI_Lon_1km[i]
+            lat = ABI_Lat_1km[i]
+
+            doy = int(timeflag[4:7])
+            hr = int(timeflag[7:9])
+            mi = int(timeflag[9:11])
+            sec = int(timeflag[11:13])
+
+            # print(doy_c,':',hr_c,':',mi_c)
+            # print('{}CLEAN DAY{}'.format('\033[1m', '\033[0m'))
+
+            minute.append(mi)
+
+            print(f'abi time is: {doy}:{hr}:{mi}')
+
+            tim = ((sec/60 + mi))/60 + hr
+            jday = doy + tim/24
+            # print('---------------')
+            # print('Fractional Day: ', round(C_jday,4),'#############','Fractional Time: ', round(C_tim,4))
+            # print('---------------------------------------------------------')
+
+            Sol_Cal = Solar_Calculate(lon, lat, jday, tim) # SZA_Calc(lon, lat, jday, tu)
+            SoZeAn.append(Sol_Cal[0])
+            SoAzAn.append(Sol_Cal[2])
+            
+            Sen_Cal = Sensor_Calculate(lon, lat)
+            SeZeAn.append(Sen_Cal[0])
+            SeAzAn.append(Sen_Cal[1])
+            print('---------------------------------------------------------')
+    ABI_Solar_Zenith_Angle = np.array(SoZeAn)
+    ABI_Solar_Azimuth_Angle = np.array(SoAzAn)
+    ABI_Sensor_Zenith_Angle = np.array(SeZeAn)
+    ABI_Sensor_Azimuth_Angle = np.array(SeAzAn)
+
+
     #save to files
     #check if there's valid data
     n_valid = np.where(select_abi_stimeflag>0)
     if ( len(n_valid[0]) <= 0 ):
         continue
-    save_name = 'ABI_G16_CAL_' + cal_timeflag + '.h5'
+    save_name = 'ABI_G16_Data_CAL_' + cal_timeflag + '.h5'
     sid = h5py.File(save_path+save_name,'w')
     sid.create_dataset('ABI_Obs',data=abi_data)
     sid.create_dataset('ABI_Timeflag',data=select_abi_stimeflag)
     sid.create_dataset('ABI_Cloud_Phase',data=abi_cloud)
-    print (f'Saved File: {save_name}.\n')
+    sid.create_dataset('ABI_Solar_Zenith_Angle', data=ABI_Solar_Zenith_Angle)
+    sid.create_dataset('ABI_Solar_Azimuth_Angle', data=ABI_Solar_Azimuth_Angle)
+    sid.create_dataset('ABI_Sensor_Zenith_Angle', data=ABI_Sensor_Zenith_Angle)
+    sid.create_dataset('ABI_Sensor_Azimuth_Angle', data=ABI_Sensor_Azimuth_Angle)
     sid.close()
